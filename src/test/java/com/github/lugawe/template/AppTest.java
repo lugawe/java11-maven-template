c09bb8999b2dd18d99a3f717610e0b6085efaf45
Luis Weich <weichluis.sw@gmail.com>
2023-02-11 17:48:32 +0100

    init

---
+++ src/test/java/com/github/lugawe/template/AppTest.java
@@ -0,0 +1,14 @@
+package com.github.lugawe.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+
+}
